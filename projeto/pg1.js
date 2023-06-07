80c915c426ee899dab0c5c73a1a56de1591fd387
iori <ioritakedo16@gmail.com>
2023-06-07 11:27:32 -0300

    up2

---
+++ projeto/pg1.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+
+    const slides = document.getElementsByClassName("mySlides");
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    if (slideIndex > slides.length - 1) {
+        slideIndex = 0;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+
+    slides[slideIndex].style.display = "block";
+
+}
